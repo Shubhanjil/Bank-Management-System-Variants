002a35f450fa117e9153bab28f382e7bd59d656b
Shubhanjil Dhuria <shubhanjild@gmail.com>
2026-03-16 09:46:25 +0530

    Added Account.cs to Simple BMS

---
+++ SimpleApplication/Account.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BankMgmtSys
+{
+    public class Account
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+    }
+}
