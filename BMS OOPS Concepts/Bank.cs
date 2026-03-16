14e044e481ff53bda9bf81b6a3829ba7b0238630
Shubhanjil Dhuria <shubhanjild@gmail.com>
2026-03-16 11:48:45 +0530

    Added Bank.cs and Program.cs

---
+++ BMS OOPS Concepts/Bank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMSwithOOPS
+{
+    public class Bank
+    {
+        private Dictionary<int, Account> accounts = new Dictionary<int, Account>();
+        // ABSTRACTION
+        public void CreateAccount(int id, string name, int initialBalance)
+        {
+            if (!accounts.ContainsKey(id))
+            {
+                accounts[id] = new Account(id, name, initialBalance);
+                Console.WriteLine("Account created successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Account ID already exists.");
+            }   
+        }
+        public void ShowAccount(int id)
+        {
+            if (accounts.ContainsKey(id))
+            {
+                accounts[id].ShowInfo();
+            }
+            else
+            {
+                Console.WriteLine("Account not found.");
+            }
+        }
+        public void Deposit(int id, int amount)
+        {
+            if (accounts.ContainsKey(id))
+            {
+                accounts[id].Deposit(amount);
+            }
+            else
+            {
+                Console.WriteLine("Account not found.");
+            }
+        }
+        public void Withdraw(int id, int amount)
+        {
+            if (accounts.ContainsKey(id))
+            {
+                accounts[id].Withdraw(amount);
+            }
+            else
+            {
+                Console.WriteLine("Account not found.");
+            }
+        }
+        public void ShowAllAccounts()
+        {
+            foreach (var kvp in accounts)
+            {
+                kvp.Value.ShowInfo();
+            }
+        }
+
+    }
+}
---
+++ BMS OOPS Concepts/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace BMSwithOOPS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Account> accounts = new Dictionary<int, Account>();
+            Bank bank = new Bank();
+            int nextId = 10001;
+            Console.WriteLine("***Welcome to Bank Management System***\n");
+            Console.WriteLine("What do you want to do:");
+            Console.WriteLine("1. Create account");
+            Console.WriteLine("2. Show account information");
+            Console.WriteLine("3. Deposit to account");
+            Console.WriteLine("4. Withdraw from account");
+            Console.WriteLine("5. Show all accounts with ids");
+            Console.WriteLine("6. Clear screen");
+            Console.WriteLine("7. Exit");
+
+            bool looping = true;
+            while (looping == true)
+            {
+                Console.Write("\nEnter Choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice - 1)
+                {
+                    case 0:
+                        Console.Write("Enter Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter Initial Balance: ");
+                        int balance = int.Parse(Console.ReadLine());
+                        int id = nextId;
+                        bank.CreateAccount(id, name, balance);
+                        nextId += 1;
+                        break;
+
+                    case 1:
+                        Console.Write("Enter Account ID: ");
+                        int showId = int.Parse(Console.ReadLine());
+                        bank.ShowAccount(showId);
+                        break;
+                    case 2:
+                        Console.Write("Enter Account ID: ");
+                        int depId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Amount to Deposit: ");
+                        int depAmount = int.Parse(Console.ReadLine());
+                        bank.Deposit(depId, depAmount);
+                        break;
+                    case 3:
+                        Console.Write("Enter Account ID: ");
+                        int wId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Amount to Withdraw: ");
+                        int wAmount = int.Parse(Console.ReadLine());
+                        bank.Withdraw(wId, wAmount);
+                        break;
+                    case 4:
+                        bank.ShowAllAccounts();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        break;
+                    case 6:
+                        looping = false;
+                        Console.WriteLine("Exiting the Application...");
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+        }
+    }
+}
